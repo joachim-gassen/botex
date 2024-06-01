b0b7e466cd8744575ab77356bc0be87d9ee2b5b9
fedossa <fikiredossa@gmail.com>
2024-06-01 10:31:54 +0200

    Implemented first attempt llm message detailed error feedback.
    Here is a first draft of opening the check_response function to provide a detailed feedback to the llm as to what it is not doing right. All imagined errors lead to a rerun and in the responses with questions it is attempted to collect all the errors and send it all at ones to the llm, so few correction calls need to be made. Finally, a max_retry of 5 is set so that the while loop is not stuck in an infinite loop.

--- src/botex/bot.py
+++ src/botex/bot.py
@@ -171,7 +171,7 @@ def run_bot(
     
     def llm_send_message(
             message, conv_hist, check_response = None, 
-            model = model, nopause = False
+            model = model, nopause = False, questions = None
         ):
         if not full_conv_history:
             conversation = [
@@ -185,7 +185,13 @@ def run_bot(
         else:
             conversation = conv_hist
         resp_dict = None
+        attempts = 0
+        max_attempts = 5
         while resp_dict is None:
+            if attempts > max_attempts:
+                logging.error("The llm did not return a valid response after %s number of attempts." % max_attempts)
+                raise Exception("Maximum number of attempts reached.")
+            attempts += 1
             conversation.append({"role": "user", "content": message})
             if not full_conv_history:
                 conv_hist.append({"role": "user", "content": message})
@@ -203,74 +209,181 @@ def run_bot(
 
             resp_str = resp.choices[0].message.content
             conv_hist.append({"role": "assistant", "content": resp_str})
+            if model == "local":
+                conversation.append({"role": "assistant", "content": resp_str})
             if resp.choices[0].finish_reason == "length":
                 logging.warning("Bot's response is too long. Trying again.")
                 message = prompts['resp_too_long']
                 continue
 
             try:
+                assert resp_str, "Bot's response is empty."
                 start = resp_str.find('{', 0)
                 end = resp_str.rfind('}', start)
                 resp_str = resp_str[start:end+1]
                 resp_dict = json.loads(resp_str, strict = False)
-                if not check_response is None:
-                    resp_dict = check_response(resp_dict)
             except:
                 logging.exception('')
                 logging.warning("Bot's response is not a valid JSON. Trying again.")
                 resp_dict = None
                 message = prompts['json_error']
                 continue
-            
-            if "error" in resp_dict:
-                resp_dict = None
-                message = prompts['confused']
+            if check_response:
+                if questions:
+                    success, error_msgs, error_logs = check_response(resp_dict, questions)
+                else:
+                    success, error_msgs, error_logs = check_response(resp_dict)
+                if not success:
+                    logging.warning(f"Detected an issue: {''.join(error_logs)}. Adjusting response.")
+                    message = ''
+                    for error_msg in error_msgs:
+                        if ':' in error_msg:
+                            error, ids = error_msg.split(": ")
+                            f_dict = {error: ids}
+                            message += prompts[error].format(**f_dict) + ' '
+                        else:
+                            message += prompts[error_msg] + ' '
+                    resp_dict = None
+                    continue
 
         return resp_dict
 
     def check_response_start(resp):
-        if "error" in resp:
-            logging.warning(f"Bot's response indicates error: '{resp['error']}'.")
-            return resp
-        if set(resp) != set(["understood"]):
-            raise RuntimeError("Bot's response does not have the required set of keys.")
+        check_result = {"error": [], "error_log": []}
+        if "error" in resp and resp["error"] != "" and resp["error"] != "None":
+            error_log = f"Bot's response indicates error: '{resp['error']}'."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("confused")
+        if 'understood' not in set(resp):
+            error_log = "Bot's response does not have the required understood key."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("no_understood_key")
         if not str(resp['understood']).lower() == "yes":
-            raise logging.warning("Bot did not understand the message.")
-        return resp
+            error_log = "Bot did not understand the message."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("not_understood")
+        if check_result.get("error"):
+            return False, check_result["error"], check_result["error_log"]
+        return True, None, None
+        
 
     def check_response_summary(resp):
-        if "error" in resp:
-            logging.warning(f"Bot's response indicates error: '{resp['error']}'.")
-            return resp
+        check_result = {"error": [], "error_log": []}
+        if "error" in resp and resp["error"] != "" and resp["error"] != "None":
+            error_log = f"Bot's response indicates error: '{resp['error']}'."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("confused")
         if set(resp) != set(["summary"]):
-            raise RuntimeError("Bot's response does not have the required set of keys.")
-        return resp
+            error_log = "Bot's response does not have the required set of keys."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("no_summary_key")
+        
+        if check_result.get("error"):
+            return False, check_result["error"], check_result["error_log"]
+        return True, None, None
 
-    def check_response_question(resp):
-        if "error" in resp:
-            logging.warning(f"Bot's response indicates error: '{resp['error']}'.")
-            return resp
-        keys = ['questions', 'summary']
-        if set(resp) != set(keys):
-            raise RuntimeError("Bot's response does not have the required set of keys.")
+    def check_response_question(resp, questions):
+        check_result = {"error": [], "error_log": []}
+        if "error" in resp and resp["error"] != "" and resp["error"] != "None":
+            error_log = f"Bot's response indicates error: '{resp['error']}'."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("confused")
+        if "summary" not in resp:
+            error_log = "Bot's response does not have a summary key"
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("no_summary_key")
+        if "questions" not in resp:
+            error_log = "Bot's response does not have a questions key"
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("no_questions_key")
+            return False, check_result["error"], check_result["error_log"]
+
         if not isinstance(resp['questions'], list):
             if isinstance(resp['questions'], dict):
                 resp['questions'] = [resp['questions']]
-            else: raise RuntimeError("Questions is not a list.")
-        for i,q in enumerate(resp['questions']):
-            if not isinstance(q, dict):
-                raise RuntimeError(f"Question {i} is not a dictionary.")
-            if set(q) != set(['id', 'answer', 'reason']):
-                raise RuntimeError(f"Question {i} does not have the required set of keys.")
-        return resp
+            else:
+                error_log = "Questions is not a list."
+                check_result["error_log"].append(error_log)
+                check_result["error"].append("questions_not_list")
+
+
+        q_ids = [q['question_id'] for q in questions] 
+        answer_ids = [q['id'] for q in resp['questions']]
+        unanswered_q_ids = set(q_ids) - set(answer_ids)
+        if unanswered_q_ids:
+            check_result["error_log"].append(f"unanswered_questions: {' '.join(unanswered_q_ids)}")
+            check_result["error"].append(f"unanswered_questions: {' '.join(unanswered_q_ids)}")
+
+        errors = {
+            "missing_answer_keys": [],
+            "missing_answer": [],
+            "missing_reason": [],
+            "answer_not_number": [],
+            "answer_not_float": [],
+            "select_answer_number": [],
+            "select_answer_unknown": []
+        }
+        for answer in resp['questions']:
+            missing_answer_keys = set(["id", "answer", "reason"]) - set(answer)
+            if missing_answer_keys:
+                errors['missing_answer_keys'].append({'question_id': answer['id'], 'missing_answer_keys': missing_answer_keys})
+            # is not empty, and can be parsed to the correct type
+            if not answer['answer']:
+                errors['missing_answer'].append(answer['id'])
+            if not answer['reason']:
+                errors['missing_reason'].append(answer['id'])
+            
+            
+            if answer['id'] in q_ids:
+                qtype = questions[q_ids.index(answer['id'])]['question_type']
+            if qtype == 'number' and isinstance(answer['answer'], str):
+                try:
+                    int_const_pattern = r'[-+]?[0-9]+'
+                    rx = re.compile(int_const_pattern, re.VERBOSE)
+                    ints = rx.findall(answer['answer'])
+                    answer = ints[0]
+                except:
+                    errors['answer_not_number'].append(answer['id'])
+            if qtype == 'float' and isinstance(answer['answer'], str):
+                try:
+                    numeric_const_pattern = r'[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
+                    rx = re.compile(numeric_const_pattern, re.VERBOSE)
+                    floats = rx.findall(answer['answer'])
+                    answer = floats[0]
+                except:
+                    errors['answer_not_float'].append(answer['id'])
+            
+            if qtype == 'select-one':
+                if not answer['answer'] in questions[q_ids.index(answer['id'])]['answer_choices']:
+                    try:
+                        int(answer['answer'])
+                        errors['select_answer_number'].append(answer['id'])
+                    except:
+                        errors['select_answer_unknown'].append(answer['id'])
+        
+        for error in errors:
+            if errors[error]:
+                check_result["error_log"].append(f"{error}: {errors[error]}")
+                check_result["error"].append(f"{error}: {errors[error]}")
+        
+        if check_result.get("error"):
+            return False, check_result["error"], check_result["error_log"]
+        return True, None, None
             
     def check_response_end(resp):
-        if "error" in resp:
-            logging.warning(f"Bot's response indicates error: '{resp['error']}'.")
-            return resp
-        if set(resp) != set(["remarks"]) in resp:
-            raise RuntimeError("Bot's response does not have the required set of keys.")
-        return resp
+        check_result = {"error": [], "error_log": []}
+        if "error" in resp and resp["error"] != "" and resp["error"] != "None":
+            error_log = f"Bot's response indicates error: '{resp['error']}'."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("confused")
+        if "remarks" not in resp:
+            error_log = "Bot's response does not have the required remarks key."
+            check_result["error_log"].append(error_log)
+            check_result["error"].append("no_remarks_key")
+        if check_result.get("error"):
+            return False, check_result["error"], check_result["error_log"]
+        return True, None, None
+
     
     conn = sqlite3.connect(botex_db)
     cursor = conn.cursor()
@@ -370,7 +483,7 @@ def run_bot(
                 """)
             message = prompts['page_not_changed'] + message
             
-        resp = llm_send_message(message, conv, check_response)
+        resp = llm_send_message(message, conv, check_response, questions=questions)
         logging.info(f"Bot analysis of page: '{resp}'")
         if not full_conv_history: summary = resp['summary']
         if questions is None and next_button is not None:
