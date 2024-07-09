acc954968a311b67d8ddfcd56d36444fb3040be4
fedossa <fikiredossa@gmail.com>
2024-07-09 21:18:40 +0200

    Clean up
    I mainly remove things that have become obsolete with the change in endpoint. Unfortunately, these were missed in the previous commit. I also refactor the doc strings to be consistent with the style in the other scripts.

--- src/botex/local_llm.py
+++ src/botex/local_llm.py
@@ -4,114 +4,9 @@ import psutil
 import requests
 import subprocess
 import time
-from typing import List, Union, Iterable
 from urllib.parse import urlparse
 
 from .gguf_parser import GGUFParser
-
-
-class Message(dict):
-    """
-    Represents a generic message within a chat history.
-
-    :param role: The role of the message sender ('user', 'assistant', or 'system').
-    :param content: The textual content of the message.
-    :raises ValueError: If content is empty or role is not one of the specified values.
-    """
-
-    def __init__(self, role: str, content: str) -> None:
-        super().__init__({"role": role, "content": content})
-        if isinstance(role, str) and role in ["user", "assistant", "system"]:
-            self.role = role
-        else:
-            raise ValueError("Role must be 'user', 'assistant' or 'system'.")
-
-    def to_text(self) -> str:
-        return f'{self.role}: {self["content"]}'
-
-
-class HumanMessage(Message):
-    """
-    Represents a message sent by a human user.
-    """
-
-    def __init__(self, content: str) -> None:
-        super().__init__("user", content)
-
-
-class AIMessage(Message):
-    """
-    Represents a message sent by an AI system.
-    """
-
-    def __init__(self, content: str) -> None:
-        super().__init__("assistant", content)
-
-
-class SystemMessage(Message):
-    """
-    Represents a message that conveys system-level information for the ai.
-    """
-
-    def __init__(self, content: str) -> None:
-        super().__init__("system", content)
-
-
-class ChatHistory(list):
-    """
-    Manages a history of messages in a conversation, facilitating tracking of dialogue context.
-
-    :param messages: A single message or a list of messages to initialize the history.
-    :raises ValueError: If messages are not in an acceptable format.
-    """
-
-    def __init__(self, messages: Union[Message, List[Message]]) -> None:
-        role_class_map = {
-            "user": HumanMessage,
-            "assistant": AIMessage,
-            "system": SystemMessage,
-        }
-        if isinstance(messages, Message):
-            messages = [messages]
-        elif isinstance(messages, list):
-            if all(isinstance(message, dict) for message in messages):
-                new_messages = []
-                for message in messages:
-                    if "role" in message and "content" in message:
-                        new_messages.append(
-                            role_class_map[message["role"]](message["content"])
-                        )
-                    else:
-                        raise ValueError(
-                            "All messages must have a 'role' and 'content' key."
-                        )
-                messages = new_messages
-            if not all(isinstance(message, Message) for message in messages):
-                raise ValueError(
-                    "All messages must be of type Message, either HumanMessage or AIMessage."
-                )
-        else:
-            raise ValueError(
-                "Invalid input. Messages must be a Message object or a list of Message objects."
-            )
-        super().__init__(messages)
-
-    def extend(self, iterable: Iterable) -> None:
-        if not all(isinstance(element, dict) for element in iterable):
-            raise ValueError(
-                "All elements in the iterable must be dictionaries with 'role' and 'content' keys."
-            )
-        for element in iterable:
-            if "role" in element and "content" in element:
-                self.append(Message(element["role"], element["content"]))
-            else:
-                raise ValueError("All messages must have a 'role' and 'content' key.")
-
-    def to_text(self) -> str:
-        chat_history = ""
-        for message in self:
-            chat_history += message.to_text() + "\\\n"
-        return chat_history
 
 
 class MirrorLiteLLMResponse:
@@ -131,12 +26,19 @@ class MirrorLiteLLMResponse:
 
 class LocalLLM:
     """
-    A class to manage local execution of a language model using a compiled main file and configuration.
+    A class to interact with the local language model server.
 
-    :param sys_prompt: The initial system prompt for the language model.
-    :param sys_prompt_few_shot_examples: Predefined few-shot examples as a part of the system prompt.
-    :param path_to_compiled_llama_cpp_main_file: Path to the compiled main file of the llama_cpp project.
-    :param model: A dictionary containing model configurations specific to the local model setup.
+    Parameters:
+    path_to_llama_server (str): The path to the llama cpp server executable.
+    local_llm_path (str): The path to the local language model.
+    api_base_url (str): The base URL for the llama cpp server.
+    context_length (int): The context length for the model, defaults to None.
+        If None, the program will try to get the context length from the local
+        model metadata, if that is not possible defaults to 4096.
+    number_of_layers_to_offload_to_gpu (int): The number of layers to offload to the GPU.
+    temperature (float): The temperature for the model, defaults to 0.5.
+    top_p (float): The top p value for the model, defaults to 0.9.
+    top_k (int): The top k value for the model, defaults to 40.
     """
 
     def __init__(
@@ -145,8 +47,6 @@ class LocalLLM:
         local_llm_path: str,
         api_base_url: str = "http://localhost:8080",
         context_length: int | None = None,
-        system_prompt_few_shot_examples: ChatHistory | None = None,
-        has_system_role: bool = False,
         number_of_layers_to_offload_to_gpu: int = 1,
         temperature: float = 0.5,
         maximum_tokens_to_predict: int = 10000,
@@ -155,23 +55,21 @@ class LocalLLM:
         num_slots: int = 1,
         **kwargs,
     ):
-        self.system_prompt_few_shot_examples = system_prompt_few_shot_examples
         self.server_path = path_to_llama_server
         self.model_path = local_llm_path
         self.api_base_url = api_base_url
         parsed_gguf = GGUFParser(self.model_path)
         self.metadata = parsed_gguf.get_metadata()
-        self.has_system_role = (
-            has_system_role
-            if isinstance(has_system_role, bool)
-            else eval(has_system_role)
+        self.ngl = int(number_of_layers_to_offload_to_gpu)
+        self.c = (
+            int(context_length)
+            if context_length
+            else self.metadata.get("context_length", 4096)
         )
-        self.ngl = number_of_layers_to_offload_to_gpu
-        self.c = int(context_length) if context_length else self.metadata.get("context_length", 4096)
         self.temp = float(temperature)
         self.n = int(maximum_tokens_to_predict)
-        self.top_p = top_p
-        self.top_k = top_k
+        self.top_p = float(top_p)
+        self.top_k = int(top_k)
         self.num_slots = num_slots
 
     def validate_parameters(self):
@@ -253,7 +151,7 @@ class LocalLLM:
                 time.sleep(1)
         return False
 
-    def stop_server(self, process: subprocess.Popen):
+    def stop_server(self, process: subprocess.Popen | None):
         """
         Stops the local language model server.
         """
@@ -269,15 +167,12 @@ class LocalLLM:
         else:
             logging.warning("Server is not running.")
 
-
     def completion(self, messages) -> MirrorLiteLLMResponse:
         """
-        Generates a completion by setting up the prompt, running the local language model, and processing its output.
+        Generates a completion for the given messages.
 
-        :param question: The question to be answered by the model.
-        :param chat_history: The current chat history for context.
-        :return: A tuple containing the generated completion and the finish reason.
-        :raises Exception: If there is an error during the execution of the model command.
+        Parameters:
+        messages (list): A list of messages to generate the completion for.
         """
 
         url = f"{self.api_base_url}/v1/chat/completions"
@@ -288,6 +183,8 @@ class LocalLLM:
             "cache_prompt": False,
             "max_tokens": self.n,
             "response_format": {"type": "json_object"},
+            "top_p": self.top_p,
+            "top_k": self.top_k,
         }
 
         attempts = 0
@@ -300,16 +197,13 @@ class LocalLLM:
                 attempts += 1
                 if attempts == 3:
                     raise Exception("Request timed out after 3 attempts.")
-                
 
         if response.status_code != 200:
             raise Exception(
                 f"An error occurred while generating the completion: {response.text}"
             )
-        
 
         completion = response.json()["choices"][0]["message"]["content"]
         finish_reason = response.json()["choices"][0]["finish_reason"]
-
 
         return MirrorLiteLLMResponse(completion, finish_reason)
